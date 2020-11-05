c57a5a38e2cec12bfd43207472482358be908472
Fergus Reig <fergusrg@gmail.com>
2020-11-05 19:15:07 +0100

    Resuelto bug mostrando nombes de capas

--- viewer/index.js
+++ viewer/index.js
@@ -1102,7 +1102,7 @@ function init(){
       onAdd: function(map) {
         this._map = map;
         var container;
-        if(varName!=null & varName!="NaN" & times.length>1){
+        if(varName!=null & varName!="NaN"){ // & times.length>1
           container = this._container = L.DomUtil.create('div', 'map_name');
           container.innerHTML = varTitle[varName];
         }else{
