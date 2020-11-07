f0026ba0ccedd416832ab27f090924f9c2069665
Fergus Reig <fergusrg@gmail.com>
2020-11-07 17:03:24 +0100

    Resuelto nuevo bug, nmo se dibujaban las gráficas

--- viewer/index.js
+++ viewer/index.js
@@ -3,30 +3,30 @@
 
 var minimumvalue = -1000;
 
-if(typeof zoom == "undefined"){
+if(typeof zoom === "undefined"){
   var zoom = 6;
 }
 // Global config
-if(typeof chooseText == "undefined"){
+if(typeof chooseText === "undefined"){
   var chooseText = "Choose Index";
 }
-if(typeof indexText == "undefined"){
+if(typeof indexText === "undefined"){
   var indexText = "Index";
 }
-if(typeof dateText == "undefined"){
+if(typeof dateText === "undefined"){
   var dateText = "Date";
 }
-if(typeof errorText == "undefined"){
+if(typeof errorText === "undefined"){
   var errorText = "Please, select a point with data";
 }
 
-if(typeof getVarMin == "undefined"){
+if(typeof getVarMin === "undefined"){
   function getVarMin(varName){
     return varMin[varName];
   }
 }
 
-if(typeof getVarMax == "undefined"){
+if(typeof getVarMax === "undefined"){
   function getVarMax(varName){
     return varMax[varName];
   }
@@ -34,7 +34,7 @@ if(typeof getVarMax == "undefined"){
 
 var defaultVarName = Object.keys(varTitle)[0]; // Id of the variable to be displayed by default
 
-if(typeof palrgb == "undefined"){
+if(typeof palrgb === "undefined"){
   function palrgb(varName){
     var palrgbArray = ["#FFFFFF", "#FFFFFD", "#FFFFFC", "#FFFFFA", "#FFFFF9", "#FFFFF8", "#FFFFF6", "#FFFFF5", "#FFFFF4", "#FFFFF2", "#FFFFF1", "#FFFFF0", "#FFFFEE", "#FFFFED", "#FFFFEC", "#FFFFEA", "#FFFFE9", "#FFFFE8", "#FFFFE6", "#FFFFE5", "#FFFFE4", "#FFFFE2", "#FFFFE1", "#FFFFE0", "#FFFFDE", "#FFFFDD", "#FFFFDC", "#FFFFDA", "#FFFFD9", "#FEFED8", "#FDFED6", "#FDFED5", "#FCFED3", "#FCFDD2", "#FBFDD1", "#FAFDCF", "#FAFDCE", "#F9FCCC", "#F8FCCB", "#F8FCC9", "#F7FCC8", "#F6FBC7", "#F6FBC5", "#F5FBC4", "#F5FBC2", "#F4FAC1", "#F3FAC0", "#F3FABE", "#F2FABD", "#F1F9BB", "#F1F9BA", "#F0F9B9", "#EFF9B7", "#EFF8B6", "#EEF8B4", "#EEF8B3", "#EDF8B1", "#ECF7B1", "#EBF7B1", "#E9F6B1", "#E8F6B1", "#E7F5B1", "#E5F5B1", "#E4F4B1", "#E3F4B1", "#E1F3B1", "#E0F3B1", "#DFF2B2", "#DDF2B2", "#DCF1B2", "#DBF0B2", "#D9F0B2", "#D8EFB2", "#D7EFB2", "#D5EEB2", "#D4EEB2", "#D3EDB3", "#D1EDB3", "#D0ECB3", "#CFECB3", "#CDEBB3", "#CCEBB3", "#CBEAB3", "#C9EAB3", "#C8E9B3", "#C7E9B4", "#C4E8B4", "#C1E7B4", "#BFE6B4", "#BCE5B4", "#BAE4B5", "#B7E3B5", "#B5E2B5", "#B2E1B5", "#B0E0B6", "#ADDFB6", "#ABDEB6", "#A8DDB6", "#A5DCB7", "#A3DBB7", "#A0DAB7", "#9ED9B7", "#9BD8B8", "#99D7B8", "#96D6B8", "#94D5B8", "#91D4B9", "#8FD3B9", "#8CD2B9", "#8AD1B9", "#87D0BA", "#84CFBA", "#82CEBA", "#7FCDBA", "#7DCCBB", "#7BCBBB", "#79CABB", "#76CABC", "#74C9BC", "#72C8BC", "#70C7BD", "#6EC6BD", "#6CC5BD", "#69C5BE", "#67C4BE", "#65C3BE", "#63C2BF", "#61C1BF", "#5EC1BF", "#5CC0BF", "#5ABFC0", "#58BEC0", "#56BDC0", "#53BDC1", "#51BCC1", "#4FBBC1", "#4DBAC2", "#4BB9C2", "#49B8C2", "#46B8C3", "#44B7C3", "#42B6C3", "#40B5C3", "#3FB4C3", "#3EB2C3", "#3CB1C3", "#3BB0C3", "#3AAFC3", "#38ADC3", "#37ACC2", "#36ABC2", "#35A9C2", "#33A8C2", "#32A7C2", "#31A5C2", "#30A4C2", "#2EA3C1", "#2DA1C1", "#2CA0C1", "#2A9FC1", "#299EC1", "#289CC1", "#279BC1", "#259AC0", "#2498C0", "#2397C0", "#2296C0", "#2094C0", "#1F93C0", "#1E92C0", "#1D91C0", "#1D8FBF", "#1D8DBE", "#1D8BBD", "#1D89BC", "#1D87BB", "#1E86BA", "#1E84BA", "#1E82B9", "#1E80B8", "#1E7FB7", "#1E7DB6", "#1F7BB5", "#1F79B4", "#1F77B4", "#1F75B3", "#1F74B2", "#2072B1", "#2070B0", "#206EAF", "#206CAF", "#206BAE", "#2069AD", "#2167AC", "#2165AB", "#2163AA", "#2162A9", "#2160A9", "#215EA8", "#225DA7", "#225BA6", "#225AA6", "#2258A5", "#2257A4", "#2255A3", "#2254A3", "#2252A2", "#2251A1", "#234FA1", "#234EA0", "#234C9F", "#234B9F", "#23499E", "#23489D", "#23469C", "#23459C", "#23439B", "#23429A", "#24409A", "#243F99", "#243D98", "#243C98", "#243A97", "#243996", "#243795", "#243695", "#243494", "#243393", "#233291", "#22328F", "#21318C", "#20308A", "#1F2F88", "#1E2E86", "#1D2E84", "#1C2D82", "#1B2C80", "#1A2B7E", "#192A7B", "#182979", "#172977", "#162875", "#152773", "#142671", "#13256F", "#12256D", "#11246B", "#102368", "#0F2266", "#0E2164", "#0D2162", "#0C2060", "#0B1F5E", "#0A1E5C", "#091D5A", "#081D58"];
     //palrgbArray = palrgbArray.reverse(); //Reverse colors
@@ -64,7 +64,7 @@ var getUrlParameter = function getUrlParameter(sParam) {
     for (i = 0; i < sURLVariables.length; i++) {
         sParameterName = sURLVariables[i].split('=');
         if (sParameterName[0] === sParam) {
-          return sParameterName[1] === undefined ? true : sParameterName[1];
+          return typeof sParameterName[1] === "undefined" ? true : sParameterName[1];
         }
     }
 };
@@ -143,10 +143,10 @@ extractCoorZoom = function(latlng, z, functionValue, int){
 
   var value = null;
   mousemoveTile = function(value){
-    if(value != undefined)
+    if(typeof value !== "undefined")
     {
       value = parseInt(value*10)/10
-      if(pointValue_ != undefined)
+      if(typeof pointValue_ !== "undefined")
       {
         value = pointValue_(value, varName)
       }
@@ -181,9 +181,9 @@ function parseDates(input) { //"28/10/50"
   return newInput;
 }
 
-if(typeof parseDate == "undefined"){
+if(typeof parseDate === "undefined"){
   function parseDate(input) { //"28/10/50"
-    if(input!=undefined){
+    if(typeof input !== "undefined"){
       input=input.replace('"', "").replace('"', "");
       if(input.indexOf('/')>-1){
         var parts = input.split('/');
@@ -341,17 +341,17 @@ function downloadCSV(x, downloadFile=true){
 }
 
 function showInfo(value) {
-  if(value!=undefined){
+  if(typeof value !== "undefined"){
     selectName = value;
   }
-  if(map_control_info!=undefined){
+  if(typeof map_control_info !== "undefined"){
     map.addControl(map_control_info);
   }
 };
 
 function removeInfo() {
   selectName = varName;
-  if(map_control_info!=undefined){
+  if(typeof map_control_info !== "undefined"){
     map.removeControl(map_control_info);
   }
 };
@@ -412,7 +412,7 @@ function getURL(bounds, done, int) {
      {
        for (var i = 0; i < 256; i++)
        {
-          if(z <= mapMaxZoom || int)//px != undefined)
+          if(z <= mapMaxZoom || int)//typeof px !== "undefined")
           {
             l = k;
           }
@@ -438,7 +438,7 @@ function getURL(bounds, done, int) {
             // Scale
             if((getVarMax(varName)[timeI] - getVarMin(varName)[timeI])!=0){
               d = floatView[0];
-              if(typeof getColor_ != "undefined"){
+              if(typeof getColor_ !== "undefined"){
                 d = getColor_(d);
               }
               l = parseInt((d - getVarMin(varName)[timeI]) / (getVarMax(varName)[timeI] - getVarMin(varName)[timeI]) * 255.0);
@@ -469,10 +469,10 @@ function getURL(bounds, done, int) {
       ctx.putImageData(imgd, 0, 0);
     }
 
-    if(px == undefined){
+    if(typeof px === "undefined"){
       done(error, canvas);
     }else{
-      if(pix != undefined){
+      if(typeof pix !== "undefined"){
           done(floatArray[px+256*py]);
       }
     }    
@@ -480,7 +480,7 @@ function getURL(bounds, done, int) {
 
   onload = function (e) {
     request.onload_(request.status, request.response, request.call);
-    if(requests[url]!=undefined){
+    if(typeof requests[url] !== "undefined"){
       delete requests[url];
     }
     for (var i = 0; i < request.call.length; i++) {
@@ -490,10 +490,10 @@ function getURL(bounds, done, int) {
     }
   }
   onerror = function (e) {
-    if(requests[url]!=undefined){
+    if(typeof requests[url] !== "undefined"){
       delete requests[url];
     }
-    if(px == undefined){
+    if(typeof px === "undefined"){
       done(error, canvas);
     }else{
       done(undefined);
@@ -530,14 +530,14 @@ function getURL(bounds, done, int) {
   request.open('GET', url, asynchronous);
   request.responseType = "arraybuffer";
 
-  if(requests[url]==undefined){
+  if(typeof requests[url] === "undefined"){
     request.send(null);
     requests[url] = request;
   }else{
     requests[url].call.push(request);
   }
 
-  if(px == undefined){
+  if(typeof px === "undefined"){
     return canvas;
   }else{
     return(undefined);
@@ -670,7 +670,7 @@ function changeMap_(value){
       return (Math.abs(curr - oldTime) < Math.abs(prev - oldTime) ? curr : prev);
     });
     timeI = newTimes.indexOf(closest);
-    if(slider!=undefined){
+    if(typeof slider !== "undefined"){
       slider.remove(map);
     }
     updateSlider();
@@ -718,12 +718,12 @@ function updateSlider(){
       getValue: function(value){
         timeI = value;
         updateCustomMap();
-        if(legend != undefined & typeof legend.remove === "function"){
+        if(typeof legend !== "undefined" & typeof legend.remove === "function"){
           legend.remove();
           legend.addTo(map);
         }
         updateURL();
-        if(typeof parseDateSlider == "undefined"){
+        if(typeof parseDateSlider === "undefined"){
           return times[varName][value];
         }else{
           return parseDateSlider(times[varName][value]);
@@ -733,7 +733,7 @@ function updateSlider(){
     slider.addTo(map);
   }else{
     updateCustomMap();
-    if(legend != undefined & typeof legend.remove === "function"){
+    if(typeof legend !== "undefined" & typeof legend.remove === "function"){
       legend.remove();
       legend.addTo(map);
     }
@@ -752,9 +752,9 @@ function updatePalette(selectName){
   return palette;
 }
 
-if(typeof getColor == "undefined"){
+if(typeof getColor === "undefined"){
   function getColor(d) {
-    // if(typeof getColor_ != "undefined"){
+    // if(typeof getColor_ !== "undefined"){
     //   d = getColor_(d);
     // }
     if(getVarMin(varName).length>1){
@@ -774,7 +774,7 @@ function init(){
   timeI = map_position;
   }
 
-  if(typeof map_name != "undefined"){
+  if(typeof map_name !== "undefined"){
     varName = map_name;
     selectName = varName;
   }
@@ -857,7 +857,7 @@ function init(){
     mouseTimeOut = setTimeout(function(){
       coor = extractCoorZoom(event.latlng, zoom, mousemoveValue)
     }, 500);
-    if(popup != undefined){
+    if(typeof popup !== "undefined"){
       map.closePopup(popup);
     }
   });
@@ -920,7 +920,7 @@ function init(){
   }
   clickPopup = returnClickPopUp();
 
-  if(typeof(showDonwloadCoordinates) == 'undefined' || showDonwloadCoordinates){
+  if(typeof(showDonwloadCoordinates) === 'undefined' || showDonwloadCoordinates){
     controlCoordinates = L.control.coordinates({
       position:"bottomleft", //optional default "bottomright" "bottomleft"
       decimals:2, //optional default 4
@@ -964,7 +964,7 @@ function init(){
 
     function grades_text(grades, i, varName){
       var text = grades[i];
-      if(grades[i + 1]==undefined){
+      if(typeof grades[i + 1] === "undefined"){
         text += '+';
       }else{
         text += '&ndash;' + grades[i + 1] + '<br>';
@@ -976,7 +976,7 @@ function init(){
     }
 
     var div = L.DomUtil.create('div', 'info legend', superdiv), grades = gradesColor, labels = [];
-    if(legendTitle[varName]!=undefined){
+    if(typeof legendTitle[varName] !== undefined){
       div.innerHTML += legendTitle[varName] + '<br>';
     }else{
       div.innerHTML += legendTitle.NaN[0] + '<br>';
@@ -1005,7 +1005,7 @@ function init(){
         var text = Object.keys(childrenObject)[i];
       }else{
         childrenObject_name = childrenObject[Object.keys(childrenObject)[i]][0]
-        if(typeof childrenObject[Object.keys(childrenObject)[i]][0] == 'undefined'){
+        if(typeof childrenObject[Object.keys(childrenObject)[i]][0] === 'undefined'){
           var childrenObject_aux = childrenObject[Object.keys(childrenObject)[i]][Object.keys(childrenObject[Object.keys(childrenObject)[i]])[0]]
           childrenObject_name = childrenObject_aux[Object.keys(childrenObject_aux)[0]]    
         }
@@ -1130,9 +1130,9 @@ function init(){
       var container = this._container = L.DomUtil.create('div', 'map_name');
       container.id = "map_info";
       container.innerHTML = "";
-      if(generalInformationNames!=undefined & generalInformation!=undefined){
+      if(typeof generalInformationNames !== "undefined" & typeof generalInformation !== "undefined"){
         for(generalInformationI = 0; generalInformationI < generalInformationNames.length; generalInformationI++) {
-          if(generalInformationNames[generalInformationI]!="null" &  generalInformation[selectName]!=undefined & generalInformation[selectName][generalInformationI]!="NA"){
+          if(generalInformationNames[generalInformationI]!="null" &  typeof generalInformation[selectName] !== "undefined" & generalInformation[selectName][generalInformationI]!="NA"){
             container.innerHTML = container.innerHTML + generalInformationNames[generalInformationI]  + ":" +  " ";
             var str = generalInformation[selectName][generalInformationI];
             str = str.replace( /(.*) \\url\{(.*)\}(.*)/, '$1 <a href="$2">$2</a>$3' )
@@ -1164,7 +1164,7 @@ function init(){
     }
   });
 
-  if(generalInformationNames!=undefined & generalInformation!=undefined){
+  if(generalInformationNames !== "undefined" & generalInformation !== "undefined"){
     map_control_info = new L.Control.Info();
   }else{
     map_control_info = undefined;
